94259a1734009c9e94570c30478e916b89782aa1
KEYRUSCORP\Tarek.Chida <tarek.chida@keyrus.com>
2018-01-24 16:47:30 +0100

    Model example

---
+++ App/Models/User.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+/**
+ * Description of User
+ *
+ * @author Tarek.Chida
+ */
+use Core\BaseModel;
+use PDO;
+
+class User extends BaseModel {
+
+    protected static $table = 'user';
+
+    public static function find($id) {
+        
+    }
+
+}
