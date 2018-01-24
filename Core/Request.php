26d2906dfee6866b5bdeb9d0730a1acfbbb1e1f3
KEYRUSCORP\Tarek.Chida <tarek.chida@keyrus.com>
2018-01-24 15:53:20 +0100

    request/response classes

---
+++ Core/Request.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Core;
+
+/**
+ * Description of Request
+ *
+ * @author Tarek.Chida
+ */
+class Request {
+
+    protected $input;
+    protected $methode;
+    protected $url;
+
+    /**
+     * 
+     * @param type $input
+     * @param type $methode
+     * @param type $url
+     */
+    public function __construct($input, $methode, $url) {
+        $this->input = $input;
+        $this->methode = $methode;
+        $this->url = $url;
+    }
+
+    /**
+     * Get request data or default value
+     * @return type
+     */
+    public function getInput($name, $default = NULL) {
+        return !empty($this->input[$name]) ? $this->input[$name] : $default;
+    }
+
+    /**
+     * 
+     * @return type
+     */
+    public function getInputArray() {
+        return $this->input;
+    }
+
+    /**
+     * 
+     * @return type
+     */
+    public function getMethode() {
+        return $this->methode;
+    }
+
+    /**
+     * 
+     * @return type
+     */
+    public function getUrl() {
+        return $this->url;
+    }
+
+    /**
+     * 
+     * @param type $input
+     */
+    public function setInput($input) {
+        $this->input = $input;
+    }
+
+    /**
+     * 
+     * @param type $methode
+     */
+    public function setMethode($methode) {
+        $this->methode = $methode;
+    }
+
+    /**
+     * 
+     * @param type $url
+     */
+    public function setUrl($url) {
+        $this->url = $url;
+    }
+
+}
